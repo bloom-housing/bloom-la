378c24df7304b95925050d975459610003c1b4e5
juan <juancarlosmillord@gmail.com>
2026-02-26 22:53:09 -0400

    feat(public): finished first section

--- sites/public/src/static_content/jurisdiction_faq_content.tsx
+++ sites/public/src/static_content/jurisdiction_faq_content.tsx
@@ -4,8 +4,61 @@ import styles from "../patterns/PageHeaderLayout.module.scss"
 import { t } from "@bloom-housing/ui-components"
 
 export const getJurisdictionFaqContent = (): FaqContent => {
-  const faqContentSection: FaqCategory = {
-    title: "Frequently Asked Questions",
+  const howShouldIPrepareForTheProcessSection: FaqCategory = {
+    title: t("faq.howShouldIPrepareForTheProcessHeader"),
+    faqs: [
+      {
+        question: t("faq.whatIdentificationWillINeed"),
+        answer: (
+          <Markdown className={styles["markdown"]} options={{ disableParsingRawHTML: true }}>
+            {t("faq.whatIdentificationWillINeedAnswer")}
+          </Markdown>
+        ),
+      },
+      {
+        question: t("faq.whatOtherPaperwork"),
+        answer: (
+          <Markdown className={styles["markdown"]} options={{ disableParsingRawHTML: true }}>
+            {t("faq.whatOtherPaperworkAnswer")}
+          </Markdown>
+        ),
+      },
+      {
+        question: t("faq.howDoIDetermineMyHouseholdSize"),
+        answer: (
+          <Markdown className={styles["markdown"]} options={{ disableParsingRawHTML: true }}>
+            {t("faq.howDoIDetermineMyHouseholdSizeAnswer")}
+          </Markdown>
+        ),
+      },
+      {
+        question: t("faq.whatSizeUnitShouldIApply"),
+        answer: (
+          <Markdown className={styles["markdown"]} options={{ disableParsingRawHTML: true }}>
+            {t("faq.whatSizeUnitShouldIApplyAnswer")}
+          </Markdown>
+        ),
+      },
+      {
+        question: t("faq.howCanImproveMyChancesOfGettingHousing"),
+        answer: (
+          <Markdown className={styles["markdown"]} options={{ disableParsingRawHTML: true }}>
+            {t("faq.howCanImproveMyChancesOfGettingHousingAnswer")}
+          </Markdown>
+        ),
+      },
+      {
+        question: t("faq.areSomeUnitsSetAsideForParticularCommunities"),
+        answer: (
+          <Markdown className={styles["markdown"]} options={{ disableParsingRawHTML: true }}>
+            {t("faq.areSomeUnitsSetAsideForParticularCommunitiesAnswer")}
+          </Markdown>
+        ),
+      },
+    ],
+  }
+  const afterIApplyWhatNextSection: FaqCategory = {
+    title: t("faq.afterIApplyWhatNextHeader"),
     faqs: [
       {
         question: t("faq.whatIdentificationWillINeed"),
@@ -51,6 +104,6 @@ export const getJurisdictionFaqContent = (): FaqContent => {
   }
 
   return {
-    categories: [faqContentSection],
+    categories: [howShouldIPrepareForTheProcessSection, afterIApplyWhatNextSection],
   }
 }
