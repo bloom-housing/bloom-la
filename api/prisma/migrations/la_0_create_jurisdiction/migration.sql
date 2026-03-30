dc82b708d9ec962929ade755dc246319c692db29
Emily Jablonski <emijablonski@gmail.com>
2026-03-30 11:43:01 -0600

    fix: pr review

--- api/prisma/migrations/la_0_create_jurisdiction/migration.sql
+++ api/prisma/migrations/la_0_create_jurisdiction/migration.sql
@@ -34,7 +34,7 @@ INSERT INTO jurisdictions (
   NULL,
   ARRAY['en','es','zh','vi','tl','ko','hy','fa']::"languages_enum"[],
   3,
-  'accesshousingla.org',
+  'https://accesshousingla.org',
   'City of Los Angeles Housing Department <lahd.AcHP.DoNotReply@lacity.org>',
   'Housing Choice Vouchers, Section 8 and other valid rental assistance programs will be considered for this property. In the case of a valid rental subsidy, the required minimum income will be based on the portion of the rent that the tenant pays after use of the subsidy.',
   '<p>If you are interested in applying for this property, please get in touch in one of these ways:</p><ul><li><p>Phone</p></li><li><p>Email</p></li><li><p>In-person</p></li><li><p>In some instances, the property has a link directly to an application</p></li></ul><p>Once you contact a property, ask if they have any available units if you are looking to move in immediately.</p><p><strong>Waitlists</strong>:</p><p>If none are available, but you are still interested in eventually living at the property, ask how you can be placed on their waitlist.</p>',
